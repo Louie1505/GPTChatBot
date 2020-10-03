ab7e6b24742108dd85b59203dea0dade67dc6ed8
Louie Harrison <louieharrison1@hotmail.co.uk>
2020-10-03 13:50:47 +0100

    First commit

---
+++ GPTChatBot/APIHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace GPTChatBot
+{
+    class APIHelper
+    {
+        public static bool CallAPI(out object oReturn, HttpVerbs verb, Type type, string controller, string id = null, string action = null, object body = null, List<WSParams> liParams = null)
+        {
+            string url = Program.configuration["APIURL"];
+            url = $"{(url.EndsWith("/") ? url : url + "/")}{controller + "/"}{(string.IsNullOrEmpty(id) ? "" : id + "/")}{(string.IsNullOrEmpty(action) ? "" : action + "/")}";
+            if (liParams != null && liParams.Count > 0)
+            {
+                url += "?";
+                for (int i = 0; i < liParams.Count; i++)
+                {
+                    if (i > 0)
+                        url += "&";
+                    url += liParams[i].URLString;
+                }
+            }
+            string key = Program.configuration["APIKEY"];
+            oReturn = null;
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("AuthID", key);
+                HttpRequestMessage requestMessage = new HttpRequestMessage(new HttpMethod(verb.ToString()), url);
+                if(verb != HttpVerbs.Get && body != null)
+                    requestMessage.Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+                HttpResponseMessage resp = client.SendAsync(requestMessage).Result;
+                if (!resp.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(resp.Content.ReadAsAsync<HttpError>().Result?.Message);
+                    return false;
+                }
+                if (!string.IsNullOrEmpty(resp.Content?.ReadAsStringAsync().Result))
+                {
+                    try
+                    {
+                        oReturn = JsonConvert.DeserializeObject(resp.Content?.ReadAsStringAsync().Result, type);
+                        Convert.ChangeType(oReturn, type);
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message + "\r\n" + e.InnerException);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+    public class WSParams
+    {
+        string Name { get; set; }
+        public string Value { get; set; }
+        public string URLString { get { return $"{Name}={Value}"; } }
+    }
+}
---
+++ GPTChatBot/Program.cs
@@ -0,0 +1,88 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GPTChatBot
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration; 
+        public static async Task Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(
+              options => options.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning))
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<Worker>()
+                  .Configure<EventLogSettings>(config =>
+                  {
+                      config.LogName = "Sample Service";
+                      config.SourceName = "Sample Service Source";
+                  });
+                // Add access to generic IConfigurationRoot
+                services.AddSingleton<IConfigurationRoot>(configuration);
+            }).UseWindowsService();
+        } 
+    }
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                new Bot().StartAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+    public class Bot
+    {
+        private DiscordSocketClient _client;
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var token = Program.configuration["Token"];
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            CommandHandler handler = new CommandHandler(_client, new Discord.Commands.CommandService());
+            await handler.InstallCommandsAsync();
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
